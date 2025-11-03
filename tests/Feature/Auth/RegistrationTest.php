4270c0a692d86345213a04abb6ec60bd73f94ace
neo <info@after.si>
2025-11-03 20:14:45 +0100

    fix: add data_processing_consent to registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -23,6 +23,7 @@ class RegistrationTest extends TestCase
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'data_processing_consent' => true,
         ]);
 
         $this->assertAuthenticated();
