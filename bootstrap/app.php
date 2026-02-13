c76a022f498b7b4a130dacbd7e34ccb29f7e6c8f
Neo <info@after.si>
2026-02-13 10:16:57 +0100

    make SetLocale middleware conditional on localization package being installed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use LaravelPlus\Localization\Middleware\SetLocale;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -23,15 +21,20 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
-        $middleware->web(append: [
+        $webMiddleware = [
             SecurityHeaders::class,
             EnsureCookieConsent::class,
             EnsureUserExists::class,
-            SetLocale::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-        ]);
+        ];
+
+        if (class_exists(\LaravelPlus\Localization\Middleware\SetLocale::class)) {
+            array_splice($webMiddleware, 3, 0, [\LaravelPlus\Localization\Middleware\SetLocale::class]);
+        }
+
+        $middleware->web(append: $webMiddleware);
 
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
